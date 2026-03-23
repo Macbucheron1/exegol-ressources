76d5c1b33667e2be36ad3fa9b1cd82ed48de1aa2
macbucheron1 <nathandeprat@hotmail.fr>
2026-03-23 11:21:36 +0100

    feat: zellij

--- setup/load_user_setup.sh
+++ setup/load_user_setup.sh
@@ -4,4 +4,9 @@ if [ -f /opt/my-resources/setup/gpu/setup-gpu.sh ]; then
   /opt/my-resources/setup/gpu/setup-gpu.sh || echo "GPU setup had errors, continuing..."
 fi
 
+cargo install --locked zellij
+rm ~/.config/zellij/config.kdl
+mkdir -p ~/.config/zellij/themes
+cp -r /opt/my-resources/setup/zellij/. ~/.config/zellij/
+
 echo "First-time setup complete!"
